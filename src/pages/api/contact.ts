8c72e3b77fee6bf2a5db338ca105a43aa505ef1f
CarlosPetersenWebDeveloper <carlos.petersen.calderon29@gmail.com>
2026-05-12 12:46:33 -0600

    done?

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,5 +1,7 @@
 import type { APIRoute } from 'astro';
 
+export const prerender = false;
+
 const RESEND_API_URL = 'https://api.resend.com/emails';
 
 function getText(value: FormDataEntryValue | null): string {
